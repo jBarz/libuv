3c96cac4e03533f5f29a1172be8beeedf885a8d2
John Barboza <jbarboza@ca.ibm.com>
2016-01-02 10:52:51 -0500

    protect epoll code from race conditions

--- src/unix/os390-epoll.h
+++ src/unix/os390-epoll.h
@@ -3,6 +3,7 @@
 
 #include <inttypes.h>
 #include <poll.h>
+#include <pthread.h>
 
 #define EPOLL_CTL_ADD 1
 #define EPOLL_CTL_DEL 2
@@ -54,6 +55,7 @@ struct epoll_event {
 struct _epoll_list{
    int size;
    struct pollfd items[MAX_ITEMS_PER_EPOLL];
+   pthread_mutex_t lock;
 };
 
 int epoll_create1(int flags);

990342de29454a3e6b9586bc3d8c1a94e79f5bf1
jbarboza <jbarboza@ca.ibm.com>
2016-05-13 16:26:06 -0400

    after rebase, we just use native POLL macros. Remove UV_POLL* stuff

--- src/unix/core.c
+++ src/unix/core.c
@@ -360,7 +360,7 @@ int uv_run(uv_loop_t* loop, uv_run_mode mode) {
     if ((mode == UV_RUN_ONCE && !ran_pending) || mode == UV_RUN_DEFAULT)
       timeout = uv_backend_timeout(loop);
 
-    //uv_print_all_handles(loop);
+    //uv_print_all_handles(loop, stdout);
 
     uv__io_poll(loop, timeout);
     uv__run_check(loop);
--- src/unix/os390-syscalls.h
+++ src/unix/os390-syscalls.h
@@ -10,14 +10,6 @@
 #define UV__EPOLL_CTL_MOD     3
 #define UV__EPOLL_CTL_ADD_MSGQ    4
 
-#define UV__EPOLLIN           1
-#define UV__EPOLLOUT          4
-#define UV__EPOLLERR          8
-#define UV__EPOLLHUP          16
-#define UV__EPOLLRDHUP        0x2000
-#define UV__EPOLLONESHOT      0x40000000
-#define UV__EPOLLET           0x80000000
-
 struct uv__epoll_event {
   uint32_t events;
   uint32_t data;
